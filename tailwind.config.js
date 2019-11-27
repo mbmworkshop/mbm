cd8fbc4d8ea3712bf57dafce956b5e90cf9f2139
Felix Held <felix.held@chalmers.se>
2019-11-27 20:14:15 +0100

    feat: navigation/header and first content
    - Added a navigation header which is fixed at the top
    - Added some dummy content
    - Some breakpoint behaviour starting at medium display size

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'mbm-green': {
+                    'dark': '#164b39',
+                    'default': '#247a5d',
+                    'light': '#91c4b3'
+                } 
+            }
+        }
+    }
+}
