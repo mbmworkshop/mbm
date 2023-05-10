78f1e6adc7149e2e0e05eb06be7b77bee65664b5
Henrik Podeus <henpo22@liu.se>
2023-05-10 14:10:34 +0200

    updated sponsor section

--- src/components/Sponsor.vue
+++ src/components/Sponsor.vue
@@ -3,9 +3,16 @@
     <div id="sponsor" class="h-0 relative" style="bottom: 100px;"></div>
     <h2 class="section-header">Sponsor</h2>
     <p>
-      This event is sponsored and made possible by . <br> 
+      This event is sponsored and made possible by AztraZeneca and the research center Cirkulation och metabolism - CircM at LiU. <br> 
     </p>
-    <img src = '../assets/AZ_CMYK_H_COL.jpg' width="30%" style="margin-top:1rem">
+    <div  style="width:100%; margin:auto;">
+      <div style="display:inline-block; width:50%;">
+        <img src = '../assets/AZ_CMYK_H_COL.jpg' width="70%" style="margin-top:1rem; display:block; margin:auto">
+      </div>
+      <div style="display:inline-block; width:50%;">
+        <img src = '../assets/CircM.png' width="50%" style="margin-top:2rem; display:block; margin:auto">
+      </div>
+    </div>
   </section>
 </template>
 
